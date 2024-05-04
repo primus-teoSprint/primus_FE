6cd0aa6bad72212faa7cd4c6d1dd8da2667b74a5
03hoho03 <ghwjd321@naver.com>
2024-05-05 06:22:36 +0900

    [#7] mswComponent 추가

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -5,15 +5,19 @@ import ReactQueryProviders from '@/components/providers/ReactQueryProviders'
 import GlobalStyle from '@/styles/GlobalStyle'
 import { theme } from '@/styles/theme'
 import { ThemeProvider } from 'styled-components'
+import { MswComponent } from './MswComponent'
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ReactQueryProviders>
-      <LayoutRegistry>
-        <GlobalStyle />
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
-      </LayoutRegistry>
-    </ReactQueryProviders>
+    <>
+      <MswComponent />
+      <ReactQueryProviders>
+        <LayoutRegistry>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </LayoutRegistry>
+      </ReactQueryProviders>
+    </>
   )
 }
 
