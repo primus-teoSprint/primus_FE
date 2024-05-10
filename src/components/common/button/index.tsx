c850dbb79a38d444585aff872e7c6fc4e8d251cc
hanseulhee <3021062@gmail.com>
2024-05-10 14:18:36 +0900

    [#4] split 버튼 컴포넌트 개발

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -8,27 +8,40 @@ import styled from 'styled-components'
 /**
  * @description 기본 버튼입니다.
  * @description status를 통해 활성화 버튼 (보라색 배경), 비활성화 버튼 (회색 배경)을 설정할 수 있습니다.
+ * @description modal에 사용될 경우 modal을 작성하면 border-radius 스타일이 변경됩니다.
  */
 
 interface ButtonProps extends ButtonAttributes {
-  name: string
   status?: Status
+  modal?: true | false
 }
 
 const Button = forwardRef(function Button(
-  { name, type = 'button', status = 'Enabled', ...rest }: ButtonProps,
+  {
+    children,
+    type = 'button',
+    status = 'Enabled',
+    modal = false,
+    ...rest
+  }: ButtonProps,
   forwardedRef: Ref<HTMLButtonElement>,
 ) {
   return (
-    <StyledWrapper type={type} ref={forwardedRef} status={status} {...rest}>
-      <span>{name}</span>
+    <StyledWrapper
+      type={type}
+      ref={forwardedRef}
+      status={status}
+      modal={modal}
+      {...rest}
+    >
+      {children}
     </StyledWrapper>
   )
 })
 
 export default Button
 
-const StyledWrapper = styled.button<Pick<ButtonProps, 'status'>>`
+const StyledWrapper = styled.button<ButtonProps>`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -36,10 +49,12 @@ const StyledWrapper = styled.button<Pick<ButtonProps, 'status'>>`
   background-color: ${({ status }) =>
     status === 'Disabled' ? theme.color.grey500 : theme.color.purple700};
   color: ${theme.color.white};
-  width: 353px;
-  height: 40px;
-  border-radius: 12px;
+  width: 350px;
+  height: 43px;
+
+  border-radius: ${({ modal }) => (modal ? '0px' : '12px')};
 
   font-size: 0.9rem;
   font-weight: ${theme.fontWeight.bold};
+  cursor: pointer;
 `
--- src/types/button.ts
+++ src/types/button.ts
@@ -3,3 +3,5 @@ import { ButtonHTMLAttributes } from 'react'
 export type ButtonAttributes = ButtonHTMLAttributes<HTMLButtonElement>
 
 export type Status = 'Enabled' | 'Disabled'
+
+export type BgColor = 'grey' | 'purple'
