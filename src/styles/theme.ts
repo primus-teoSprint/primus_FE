60e43f252c290a3cd31b1ef39b27cc996f01648a
03hoho03 <ghwjd321@naver.com>
2024-05-11 12:52:30 +0900

    [#9] grey색 추가 및 수정 fontWeight 수정, NanumFontFamily 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,15 +6,25 @@ export const theme = {
     purple100: '#f3f4ff',
     purple700: '#6c76f4',
     grey100: '#fbfbfb',
+    grey200: '#eeeeee',
     grey300: '#ECECEC',
-    grey500: '#D9D9D9',
+    grey400: '#D9D9D9',
+    grey500: '#c9c9c9',
+    grey600: '#9f9f9f',
     grey700: '#989898',
+    red: '#ff0000',
   },
   fontWeight: {
     light: 300,
-    normal: 500,
+    normal: 400, // regular
     bold: 700,
-    extraBold: 900,
+    extraBold: 800,
+  },
+  NanumFontFamily: {
+    light: 'NanumSquareLight',
+    normal: 'NanumSquare', // regular
+    bold: 'NanumSquareBold',
+    extraBold: 'NanumSquareExtraBold',
   },
 } as const
 
