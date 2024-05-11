eb395fb3c8622898f8d44d0755fc3e25869b5d1a
03hoho03 <ghwjd321@naver.com>
2024-05-11 12:50:59 +0900

    [#9] component props에 css prop 추가

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,6 +1,11 @@
 import { Theme } from '@/styles/theme'
-import 'styled-components'
+import type { CSSProp } from 'styled-components'
 
+declare module 'react' {
+  interface Attributes {
+    css?: CSSProp
+  }
+}
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {}
 }
